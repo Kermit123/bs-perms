763cab5c82518838e4c763b9453c13814453f38b
Ryan Bucshon <busheezy@gmail.com>
2017-05-14 12:26:54 -0500

    I don't need this

--- server/utilities.lua
+++ server/utilities.lua
@@ -1,11 +1,3 @@
-function splitBySpace(stringToSplit)
-  local args = {}
-  for arg in string.gmatch(stringToSplit, "%S+") do
-    args[#args + 1] = arg
-  end
-  return args
-end
-
 function startsWith(theString, start)
   return string.sub(theString, 1, string.len(start)) == start
 end
