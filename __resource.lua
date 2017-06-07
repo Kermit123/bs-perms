34316632530b865a3dfb85bce23b2a954a2fb4c0
Ryan Bucshon <busheezy@gmail.com>
2017-06-07 03:35:18 -0500

    remove manifest until I'm not tired

--- __resource.lua
+++ __resource.lua
@@ -5,8 +5,6 @@ maintainer 'BuSheeZy <https://github.com/busheezy>'
 
 version '1.0.2'
 
-resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
-
 server_script 'server/utilities.lua'
 server_script 'server/server.lua'
 server_script 'server/chat_processor.lua'
